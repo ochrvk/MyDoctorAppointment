2066ba4d1a950f3f4c320fecc51290dff830a638
Oleksandr Cherevko <oc.oleks.cherevko@gmail.com>
2023-09-07 03:55:58 +0300

    First project build

--- MyDoctorAppointment/Program.cs
+++ MyDoctorAppointment/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MyDoctorAppointment.Service.Interfaces;
+using MyDoctorAppointment.Service.Services;
+
+namespace MyDoctorAppointment
+{
+    public class DoctorAppointment
+    {
+        private readonly IDoctorService _doctorService;
+
+        public DoctorAppointment()
+        {
+            _doctorService = new DoctorService();
+        }
+
+        public void Menu()
+        {
+            Console.WriteLine("Current doctors list: ");
+            var docs = _doctorService.GetAll();
+
+            foreach(var doc in docs)
+            {
+                Console.WriteLine(doc.Name);
+
+            }
+        }
+    }
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            var doctorAppointment = new DoctorAppointment();
+        }
+    }
+}
